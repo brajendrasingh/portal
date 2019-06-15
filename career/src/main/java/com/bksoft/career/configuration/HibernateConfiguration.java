17511bdefcea0274f23bf3133801061a926ae710
brajendrasingh <brajendrasingh3clogic@gmail.com>
2019-06-15 17:40:42 +0530

    Adding HibernateConfiguration

---
+++ career/src/main/java/com/bksoft/career/configuration/HibernateConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.bksoft.career.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+/**
+ * The Class ConfigurationProvider.
+ *
+ * @author BRAJENDRA SINGH
+ */
+@Configuration
+public class HibernateConfiguration {
+
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("com.bksoft.entities");
+		return sessionFactory;
+	}
+
+	@Bean
+	public HibernateTransactionManager transactionManager() {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory().getObject());
+		return txManager;
+	}
+}
