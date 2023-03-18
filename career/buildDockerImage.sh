df3f8e2b0081e4cb40cf5c74e50758529ed1dc31
brajendrasingh <brajendrasingh3clogic@gmail.com>
2023-03-18 14:12:23 +0530

    Create buildDockerImage.sh

---
+++ career/buildDockerImage.sh
@@ -0,0 +1,5 @@
+rm -rf career-service.tar
+docker rmi -f career-service:1.0
+docker build -F Dockerfile -t career-service:1.0
+docker save -o career-service.tar career-service:1.0
+chmod 755 career-service.tar
