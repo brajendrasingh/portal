85d72fdf38668ba48d13cfb37a9347355f35a0d8
brajendra <brajendrasingh3clogic@gmail.com>
2025-04-12 10:44:46 +0530

    feat: environment url updated in services

--- web-portal/src/app/core/services/auth.service.ts
+++ web-portal/src/app/core/services/auth.service.ts
@@ -2,22 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-import { Constants } from '../constants';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  
    constructor(private http: HttpClient, private router: Router) {}
 
   login(email: string, password: string): Observable<any> {
     //const mockurl = './../../../assets/mock/user/mock-user.json';
-    return this.http.post(Constants.BASE_URL + '/auth/login', { email, password });
+    return this.http.post(`${environment.apiUrl}/auth/login`, { email, password });
   }
 
   signup(user: any) {
     //const mockurl = './../../../assets/mock/user/mock-user.json';
-    return this.http.post(Constants.BASE_URL + '/auth/signup', user);
+    return this.http.post(`${environment.apiUrl}/auth/signup`, user);
   }
 
   logout() {
