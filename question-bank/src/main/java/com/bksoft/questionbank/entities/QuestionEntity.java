bcde56c297cc91d5e570affd7395937b7541026b
brajendra <brajendrasingh3clogic@gmail.com>
2026-01-27 20:48:28 +0530

    models refactoring

--- question-bank/src/main/java/com/bksoft/questionbank/entities/QuestionEntity.java
+++ question-bank/src/main/java/com/bksoft/questionbank/entities/QuestionEntity.java
@@ -1,9 +1,13 @@
 package com.bksoft.questionbank.entities;
 
+import com.bksoft.questionbank.dtos.QuestionCategory;
+import com.bksoft.questionbank.dtos.QuestionDifficulty;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document(collection = "questions")
 @Builder
@@ -19,4 +23,12 @@ public class QuestionEntity {
 	private String optionD;	
 	private String optionE;	
 	private String correctOption;
+	private String correctAnswer;
+	private List<String> answerOptions;
+	private String subject;
+	private String topic;
+	private QuestionCategory category;
+	private Integer marks;
+	private QuestionDifficulty difficulty;
+	private String explanation;
 }
