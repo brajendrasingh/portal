52856b1e19a2f320702d09d359298ad9563f936f
brajendra <brajendrasingh3clogic@gmail.com>
2026-02-21 15:36:48 +0530

    refactoring

--- question-bank/src/main/java/com/bksoft/questionbank/service/AssessmentSubmissionService.java
+++ question-bank/src/main/java/com/bksoft/questionbank/service/AssessmentSubmissionService.java
@@ -3,7 +3,6 @@ package com.bksoft.questionbank.service;
 import com.bksoft.questionbank.api.models.AssessmentSubmissionRequest;
 import com.bksoft.questionbank.api.models.SubmittedAnswerDetail;
 import com.bksoft.questionbank.entities.AssessmentSubmission;
-import com.bksoft.questionbank.entities.QuestionEntity;
 import com.bksoft.questionbank.entities.QuestionResponse;
 import com.bksoft.questionbank.entities.i18n.Question;
 import com.bksoft.questionbank.repositories.AssessmentSubmissionRepository;
@@ -16,10 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -120,11 +116,27 @@ public class AssessmentSubmissionService {
     }
 
     public Page<SubmittedAnswerDetail.QuestionAnswer> getSubmittedAnswerDetail(String userId, String assessmentId, Integer attemptNo, int page, int size) {
-
-        Pageable pageable = PageRequest.of(page, size, Sort.by("submittedAt").descending());
-
-
-        return null;
+        AssessmentSubmission res = submissionRepo.findByUserIdAndAssessmentIdAndAttemptNo(userId,assessmentId, attemptNo).orElseThrow(() -> new EntityNotFoundException("Submission not found"));
+        List<QuestionResponse> responses = res.getResponses();
+        Pageable pageable = PageRequest.of(page, size);
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), responses.size());
+        if (start > responses.size()) {
+            return new PageImpl<>(Collections.emptyList(), pageable, responses.size());
+        }
+        List<QuestionResponse> pagedResponses = responses.subList(start, end);
+        List<SubmittedAnswerDetail.QuestionAnswer> questionAnswers = new ArrayList<>();
+        for (QuestionResponse qr : pagedResponses) {
+            Question q = questionRepository.findById(qr.getQuestionId()).orElseThrow(() -> new EntityNotFoundException("Question not found"));
+            SubmittedAnswerDetail.QuestionAnswer qa = new SubmittedAnswerDetail.QuestionAnswer();
+            qa.questionId = qr.getQuestionId();
+            qa.questionText = q.getTranslations().get(0).getQuestionText();
+            qa.correctAnswers = List.of(q.getTranslations().get(0).getOptions().get(q.getCorrectIndex()).getOptionText());
+            qa.questionType = qr.getQuestionType();
+            qa.selectedAnswers = qr.getSelectedAnswers();
+            questionAnswers.add(qa);
+        }
+        return new PageImpl<>(questionAnswers, pageable, responses.size());
     }
 
     public List<AssessmentSubmission> getAllSubmissions(String userId, String assessmentId) {
