bb137f0a115a0e1465f091a2df7a3d4e25673d89
brajendra <brajendrasingh3clogic@gmail.com>
2026-03-07 11:58:22 +0530

    liquibase required deps changes

--- liquibase-migration-tool/build.gradle.kts
+++ liquibase-migration-tool/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.liquibase:liquibase-core:5.0.1")
+        classpath("info.picocli:picocli:4.7.5")
+    }
+}
+
 plugins {
     java
     id("org.liquibase.gradle") version "3.1.0"
@@ -15,13 +25,16 @@ dependencies {
 
     liquibaseRuntime("org.liquibase:liquibase-core:5.0.1")
     liquibaseRuntime("mysql:mysql-connector-java:8.0.33")
+    //Required for liquibase 4.30.0+ versions
+    liquibaseRuntime("org.apache.commons:commons-lang3:3.14.0")
+    liquibaseRuntime("info.picocli:picocli:4.7.5")
 }
 
 liquibase {
     activities.register("main") {
         arguments = mapOf(
             "changelogFile" to "src/main/resources/db/changelog/db.changelog-master.xml",
-            "url" to "jdbc:mysql://127.0.0.1:3306/assessmentdb",
+            "url" to "jdbc:mysql://127.0.0.1:3306/liquibase-db",
             "username" to "root",
             "password" to "root",
             "driver" to "com.mysql.cj.jdbc.Driver"
--- liquibase-migration-tool/src/main/java/Migration.java
+++ liquibase-migration-tool/src/main/java/Migration.java
@@ -8,23 +8,19 @@ import liquibase.database.jvm.JdbcConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class LiquibaseRunner {
+public class Migration {
 
     public static void main(String[] args) {
-
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "root");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/liquibase-db", "root", "root");
 
-            Database database = DatabaseFactory.getInstance()
-                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
 
-            Liquibase liquibase = new Liquibase("db/changelog/db.changelog-master.xml",
-                    new ClassLoaderResourceAccessor(), database);
+            Liquibase liquibase = new Liquibase("db/changelog/db.changelog-master.xml", new ClassLoaderResourceAccessor(), database);
 
             liquibase.update();
 
             System.out.println("Migration completed successfully");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
